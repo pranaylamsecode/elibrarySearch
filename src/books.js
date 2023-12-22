f50c0bbd29152a17a449adffe04fce0623033147
pranaylamsecode <pranaylamsecode@gmail.com>
2023-12-22 14:52:50 +0530

    added change related to api response

--- src/books.js
+++ src/books.js
@@ -1,39 +1,153 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import logo from "./img/Book.svg";
-import Searchform from "./searchform";
+
 import Footer from "./footer";
 import Book from "./book";
 import LoadingCard from "./loadingCard";
+import Select from "react-select";
 
 const BookDetails = () => {
+  /* variable for search */
+
   const [details, setDetails] = useState([]);
   const [prevLimit, setPrevLimit] = useState(10);
   const [prevSkip, setPrevSkip] = useState(0);
-  const [genre, setGenre] = useState(18);
-  const [library_id, setLibraryId] = useState(0);
   const [term, setTerm] = useState("");
-
   const [isLoading, setIsLoading] = useState(true);
+  const [text, setText] = useState("");
+  const [showValidTextModal, setShowValidTextModal] = useState(false);
+  const [genres, setGenre] = useState([]);
+  const [authors, setAuthor] = useState([]);
+  const [publishers, setPublisher] = useState([]);
+  const [languages, setLanguage] = useState([]);
+  const [formats, setFormat] = useState([]);
+  const [searchText, setsearchText] = useState([]);
+  const [library, setLibrary] = useState([
+    { value: 111, label: "dindayalupadhyay" },
+    { value: 222, label: "kundanlalgupta" },
+    { value: 333, label: "rashtramatakasturba" },
+  ]);
+
+  const [formgenre, setformGenre] = useState(0);
+
+  const [formauthors, setformauthors] = useState(0);
+  const [formpublishers, setformpublishers] = useState(0);
+  const [formlanguages, setformlanguages] = useState(0);
+  const [formformats, setformformats] = useState(0);
+
+  const [formlibrary_id, setLibraryId] = useState(0);
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
+  useEffect(() => {
+    const fetchGenre = async () => {
+      const resources = await axios.get(
+        `https://dindayalupadhyay.smartcitylibrary.com/api/v1/genres?order_by=name&direction=asc`
+      );
+      setGenre(resources.data.data);
+    };
+    const fetchAuthor = async () => {
+      const resources = await axios.get(
+        `https://dindayalupadhyay.smartcitylibrary.com/api/v1/authors?order_by=first_name&direction=asc`
+      );
+      setAuthor(resources.data.data);
+    };
+    const fetchPublisher = async () => {
+      const resources = await axios.get(
+        `https://dindayalupadhyay.smartcitylibrary.com/api/v1/publishers?order_by=name&direction=asc`
+      );
+      setPublisher(resources.data.data);
+    };
+    const fetchLanguage = async () => {
+      const resources = await axios.get(
+        `https://dindayalupadhyay.smartcitylibrary.com/api/b1/book-languages?order_by=language_name&direction=asc`
+      );
+      setLanguage(resources.data.data);
+    };
+    const fetchFormat = async () => {
+      setFormat([
+        { value: 1, label: "Hardcover" },
+        { value: 2, label: "PaperBack" },
+        { value: 3, label: "E-Book" },
+      ]);
+    };
+    fetchGenre();
+    fetchAuthor();
+    fetchPublisher();
+    fetchLanguage();
+    fetchFormat();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text === "" || !text.trim()) {
+      setShowValidTextModal(true);
+      return;
+    }
+    searchText(text);
+  };
+
+  const onChangevalue = (e) => {
+    e.preventDefault();
+    setText(e.target.value);
+    searchText(e.target.value);
+    if (e.target.value === "") {
+      setText("");
+      searchText("");
+    }
+  };
+
+  const filterformGenre = (e) => {
+    setformGenre(e.value);
+  };
+  const filterformAuthor = (e) => {
+    setformauthors(e.value);
+  };
+  const filterformPublishers = (e) => {
+    setformpublishers(e.value);
+  };
+  const filterformLanguages = (e) => {
+    setformlanguages(e.value);
+  };
+  const filterformFormat = (e) => {
+    setformformats(e.value);
+  };
+
+  const filterformLibrary = (e) => {
+    setLibraryId(e.value);
+  };
+
+  /* variable for search end */
 
   useEffect(() => {
     const fetchDetails = async () => {
       setIsLoading(true);
       const resources = await axios.get(
-        `https://dindayalupadhyay.smartcitylibrary.com/api/v1/books?order_by=created_at&direction=desc&limit=10&skip=0&search=${term}&genre=${genre}&library_id=${library_id}`
+        `https://dindayalupadhyay.smartcitylibrary.com/api/v1/books?order_by=created_at&direction=desc&limit=10&skip=0&search=${term}&genre=${formgenre}&library_id=${formlibrary_id}&author=${formauthors}&publisher=${formpublishers}&language=${formlanguages}&format=${formformats}`
       );
       setDetails(resources.data.data);
       setIsLoading(false);
     };
     fetchDetails();
-  }, [term]);
+  }, [
+    term,
+    formgenre,
+    formauthors,
+    formpublishers,
+    formlanguages,
+    formformats,
+    formlibrary_id,
+  ]);
 
   const loadMore = async () => {
     setPrevLimit(prevLimit + 10);
     setPrevSkip(prevSkip + 10);
 
     const resources = await axios.get(
-      `https://dindayalupadhyay.smartcitylibrary.com/api/v1/books?order_by=created_at&direction=desc&limit=${prevLimit}&skip=${prevSkip}&search=${term}&genre=${genre}&library_id=${library_id}`
+      `https://dindayalupadhyay.smartcitylibrary.com/api/v1/books?order_by=created_at&direction=desc&limit=10&skip=0&search=${term}&genre=${formgenre}&library_id=${formlibrary_id}&author=${formauthors}&publisher=${formpublishers}&language=${formlanguages}&format=${formformats}`
     );
     setDetails((oldDetails) => [...oldDetails, ...resources.data.data]);
   };
@@ -52,7 +166,167 @@ const BookDetails = () => {
       >
         {term}
       </h2>
-      <Searchform searchText={(text) => setTerm(text)}></Searchform>
+      {/* saerch logic  */}
+      <div>
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Search by book and author name"
+            onChange={onChangevalue}
+          />
+          <button
+            style={{
+              marginLeft: "1rem",
+              transition: "all 0.3s ease 0s",
+              padding: "0.6rem",
+              borderRadius: "0.2rem",
+              cursor: "pointer",
+            }}
+            type="submit"
+          >
+            Search
+          </button>
+
+          <a
+            style={{
+              marginLeft: "1rem",
+              transition: "all 0.3s ease 0s",
+              padding: "0.6rem",
+              borderRadius: "0.2rem",
+              cursor: "pointer",
+            }}
+            onClick={refreshPage}
+          >
+            <span style={{ color: "red" }}>Reset</span>
+          </a>
+        </form>
+
+        <br />
+        <div className="row">
+          <div className="col-md-12 filter-row">
+            <div className="col-md-3">
+              <Select
+                title="Genre"
+                placeholder="Select Genre"
+                options={
+                  genres
+                    ? genres.map((genre, i) => ({
+                        label: genre.name,
+                        value: genre.id,
+                      }))
+                    : []
+                }
+                onChange={filterformGenre}
+              />
+            </div>
+
+            <div className="col-md-3">
+              <Select
+                title="Author"
+                placeholder="Select Author"
+                options={
+                  authors
+                    ? authors.map((author, i) => ({
+                        label: author.first_name,
+                        value: author.id,
+                      }))
+                    : []
+                }
+                onChange={filterformAuthor}
+              />
+            </div>
+
+            <div className="col-md-3">
+              <Select
+                title="Publishers"
+                placeholder="Select Publishers"
+                options={
+                  publishers
+                    ? publishers.map((publisher, i) => ({
+                        label: publisher.name,
+                        value: publisher.id,
+                      }))
+                    : []
+                }
+                onChange={filterformPublishers}
+              />
+            </div>
+
+            <div className="col-md-3">
+              <Select
+                title="Languages"
+                placeholder="Select Languages"
+                options={
+                  languages
+                    ? languages.map((language, i) => ({
+                        label: language.language_name,
+                        value: language.id,
+                      }))
+                    : []
+                }
+                onChange={filterformLanguages}
+              />
+            </div>
+
+            <br />
+            <br />
+
+            <div className="col-md-3">
+              <Select
+                title="Formats"
+                placeholder="Select Formats"
+                options={
+                  formats
+                    ? formats.map((genre, i) => ({
+                        label: genre.label,
+                        value: genre.value,
+                      }))
+                    : []
+                }
+                onChange={filterformFormat}
+              />
+            </div>
+
+            <div className="col-md-3">
+              <Select
+                title="Library"
+                placeholder="Select Library"
+                options={
+                  library
+                    ? library.map((genre, i) => ({
+                        label: genre.label,
+                        value: genre.value,
+                      }))
+                    : []
+                }
+                onChange={filterformLibrary}
+              />
+            </div>
+          </div>
+        </div>
+
+        <br />
+        <div
+          id="popup1"
+          class={showValidTextModal ? "overlay modal-active" : "overlay"}
+        >
+          <div class="popup">
+            <div class="close" onClick={() => setShowValidTextModal(false)}>
+              &times;
+            </div>
+            <h3 class="content">Please Enter the valid text</h3>
+          </div>
+        </div>
+      </div>
+
+      {/* search logic end  */}
       {isLoading ? (
         <section className="container" style={{ padding: "2rem 0rem" }}>
           <LoadingCard />
